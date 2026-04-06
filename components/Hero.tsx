003f1e37d8c08053620e1dc0c6645e8d81ec9286
0xde7c <0xde7c@gmail.com>
2026-04-06 18:59:56 +0800

    Remove flip and adjust height to 13.5rem to touch first line perfectly

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Hero() {
   return (
     <section className="py-20 pb-16 border-b border-slate-200 relative overflow-hidden">
-      <div style={{position: 'absolute', top: '0', right: '0', width: '70%', height: '14rem', pointerEvents: 'none', transform: 'scaleX(-1)'}}>
+      <div style={{position: 'absolute', top: '0', right: '0', width: '70%', height: '13.5rem', pointerEvents: 'none'}}>
         <Image
           src="/pattern-dark.png"
           alt=""
